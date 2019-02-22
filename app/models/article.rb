34960261616fcf0120e32a4e1dd3cfa88f2832ef
Bharath Prabhu <bharathp@andrew.cmu.edu>
2019-02-22 19:10:14 +0000

    generated article model and wrote relationships, scopes and validations

---
+++ app/models/article.rb
@@ -0,0 +1,12 @@
+class Article < ApplicationRecord
+  # relationships
+  belongs_to :category
+  
+  # contents
+  validates_presence_of :title
+  validates_presence_of :content
+  
+  # scopes
+  scope :alphabetical, -> { order('title') }
+  scope :active, 			 -> { where('active = ?', true) }
+end
